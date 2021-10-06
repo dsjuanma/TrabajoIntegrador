d0f3acbc5f77ef61a912a4ff27f2f2809fb1d81f
dsjuanma <juanmanuel_dasilva@hotmail.com>
2021-10-06 20:49:33 -0300

    Modificacion en el config para que se redirija a la carpeta correspondiente

--- Config/Config.php
+++ Config/Config.php
@@ -1,8 +1,8 @@
 <?php namespace Config;
 
 define("ROOT", dirname(__DIR__) . "/");
-//Path to your project's root folder
-define("FRONT_ROOT", "/UTN/LabIV/Framework/");
+// IMPORTANT Path to your project's root folder
+define("FRONT_ROOT", "/TrabajoIntegrador/");
 define("VIEWS_PATH", "Views/");
 define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
 define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
