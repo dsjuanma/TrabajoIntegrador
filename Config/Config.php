df905f7cfad766421f4426f942a738b578cf65b8
dsjuanma <juanmanuel_dasilva@hotmail.com>
2021-10-07 19:08:48 -0300

    index se crean la conexion hacia la api

--- Config/Config.php
+++ Config/Config.php
@@ -6,6 +6,8 @@ define("FRONT_ROOT", "/TrabajoIntegrador/");
 define("VIEWS_PATH", "Views/");
 define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
 define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
+// student's api key
+define('API-KEY','4f3bceed-50ba-4461-a910-518598664c08');
 ?>
 
 
--- Views/index.php
+++ Views/index.php
@@ -5,7 +5,31 @@
      <section id="listado" class="mb-5">
           <div class="container">
                <h2 class="mb-4">index</h2>
+
+               <?php
+               //Starts a new session and returns the curl handler for using the functions
+               $apiStudent = curl_init();
+
+               //Sets an option in the session of the cURL resource.
+               //1- Resource 2- The CURLOPT_XXX option to configure. 3- Consumed URL
+               curl_setopt($apiStudent,CURLOPT_URL,'https://utn-students-api.herokuapp.com/api/Student');
+
+               curl_setopt($apiStudent,CURLOPT_HTTPHEADER,array('x-api-key: 4f3bceed-50ba-4461-a910-518598664c08'));
+
+               // Shipping request
+
+
+               //Execute the cURL session that is passed to it as a parameter.
+               $response = curl_exec($apiStudent);
+
+               // Api Key
+               $api_key = '4f3bceed-50ba-4461-a910-518598664c08';
+
+               echo($response);
+
+               ?>
+            
              
           </div>
      </section>
-</main>
+</main>;
