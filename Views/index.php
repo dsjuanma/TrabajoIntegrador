29d68677094c4d7fdbbf242666ee7bc17e911d52
dsjuanma <juanmanuel_dasilva@hotmail.com>
2021-10-06 21:05:47 -0300

    Eliminando el form del index que pedia cargar un estudiante

--- Views/index.php
+++ Views/index.php
@@ -4,30 +4,8 @@
 <main class="py-5">
      <section id="listado" class="mb-5">
           <div class="container">
-               <h2 class="mb-4">Agregar alumno</h2>
-               <form action="<?php echo FRONT_ROOT ?>Student/Add" method="post" class="bg-light-alpha p-5">
-                    <div class="row">                         
-                         <div class="col-lg-4">
-                              <div class="form-group">
-                                   <label for="">Legajo</label>
-                                   <input type="text" name="recordId" value="" class="form-control">
-                              </div>
-                         </div>
-                         <div class="col-lg-4">
-                              <div class="form-group">
-                                   <label for="">Nombre</label>
-                                   <input type="text" name="firstName" value="" class="form-control">
-                              </div>
-                         </div>
-                         <div class="col-lg-4">
-                              <div class="form-group">
-                                   <label for="">Apellido</label>
-                                   <input type="text" name="lastName" value="" class="form-control">
-                              </div>
-                         </div>
-                    </div>
-                    <button type="submit" name="button" class="btn btn-dark ml-auto d-block">Agregar</button>
-               </form>
+               <h2 class="mb-4">index</h2>
+             
           </div>
      </section>
 </main>
